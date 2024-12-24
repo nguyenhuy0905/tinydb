b9e6bd126225ad629321e7825e4b523ef5e70099
Huy Nguyen <huy55465@gmail.com>
2024-12-24 00:35:23 -0500

    add column and table meta
    In the next day I will probably work on writing and reading the table
    metadata from a file.
    
    commit-by: Huy Nguyen

--- src/dbfile/tbl.hxx
+++ src/dbfile/tbl.hxx
@@ -2,6 +2,7 @@
 #define TINYDB_DBFILE_TBL_HXX
 
 #include <cstdint>
+#include <filesystem>
 #include <string>
 #include <unordered_map>
 
@@ -10,16 +11,41 @@ namespace tinydb::dbfile {
 using ColID = uint8_t;
 using EntrySiz = uint16_t;
 
-struct TableEntry {
+/**
+ * @class ColumnMeta
+ * @brief Metadata of one column
+ *
+ */
+struct ColumnMeta {
     std::string m_name;
-    uint16_t m_size;
+    EntrySiz m_size;
     ColID m_col_id;
+    EntrySiz m_offset;
 };
 
-class Table {
-    std::unordered_map<ColID, TableEntry> m_entries;
+/**
+ * @class Table
+ * @brief The table meta contains a bunch of column metas.
+ *
+ */
+class TableMeta {
+  public:
+    TableMeta() = default;
+    static auto read_from(std::filesystem::path t_path) -> TableMeta;
+    auto add_column(ColumnMeta&& t_colmeta);
+    auto remove_column(ColID t_id);
+    /**
+     * @brief Overwrite the content of a file with the table metadata.
+     *
+     * @param t_path The path to write to.
+     */
+    auto write_to(std::filesystem::path t_path);
+
+  private:
+    std::unordered_map<ColID, ColumnMeta> m_entries;
+    std::string m_name;
 };
 
-}
+} // namespace tinydb::dbfile
 
 #endif // !TINYDB_DBFILE_TBL_HXX
