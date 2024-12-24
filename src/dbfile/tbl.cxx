fe43fc89077e4641f0a0f98d4e50bee5335aeeaf
Huy Nguyen <huy55465@gmail.com>
2024-12-24 11:45:27 -0500

    still working on it

--- src/dbfile/tbl.cxx
+++ src/dbfile/tbl.cxx
@@ -1,1 +1,29 @@
 #include "tbl.hxx"
+#include <filesystem>
+#include <fstream>
+#include <ios>
+#include <ranges>
+
+namespace tinydb::dbfile {
+
+void TableMeta::write_to(const std::filesystem::path& path) {
+    std::ofstream file{path, std::ios::binary | std::ios::trunc};
+    file.exceptions(std::ofstream::failbit);
+
+    file.write(m_name.c_str(), static_cast<std::streamsize>(m_name.length()))
+        .put('{');
+
+    for (const ColumnMeta& colmeta :
+         m_entries | std::views::transform(
+                         [](const auto pair) { return pair.second; })) {
+        file.write(colmeta.m_name.c_str(),
+                   static_cast<std::streamsize>(colmeta.m_name.length()))
+            .put(' ')
+            // this seems to be necessary to write binary into a file
+            //NOLINTBEGIN(*-reinterpret-cast)
+            .write(reinterpret_cast<const char*>(colmeta.m_col_id),
+                   sizeof(colmeta.m_col_id));
+            //NOLINTEND(*-reinterpret-cast)
+    }
+}
+} // namespace tinydb::dbfile
--- src/dbfile/tbl.hxx
+++ src/dbfile/tbl.hxx
@@ -3,8 +3,11 @@
 
 #include <cstdint>
 #include <filesystem>
+#include <map>
+#include <optional>
+#include <stdexcept>
 #include <string>
-#include <unordered_map>
+#include <type_traits>
 
 namespace tinydb::dbfile {
 
@@ -29,20 +32,59 @@ struct ColumnMeta {
  *
  */
 class TableMeta {
+    static_assert(!std::is_trivially_copyable_v<std::filesystem::path>);
+    static_assert(!std::is_trivially_copyable_v<ColumnMeta>);
+
   public:
+    template <typename T> using rw = std::reference_wrapper<T>;
     TableMeta() = default;
-    static auto read_from(std::filesystem::path t_path) -> TableMeta;
-    auto add_column(ColumnMeta&& t_colmeta);
-    auto remove_column(ColID t_id);
+    static auto read_from(const std::filesystem::path& t_path) -> TableMeta;
+    /**
+     * @brief I changed this to noexcept since I think the lookup function
+     * should not throw an exception.
+     * @return The column meta with the specified ID, or nullopt if there isn't
+     * one
+     */
+    auto get_column(ColID t_id) noexcept
+        -> std::optional<rw<const ColumnMeta>> const {
+        try {
+            return {{m_entries.at(t_id)}};
+        } catch (std::out_of_range e) {
+            return {std::nullopt};
+        }
+    }
+    /**
+     * @brief Inserts a column into the table.
+     *
+     * @tparam T ColumnMeta or references (lvalue or rvalue) to ColumnMeta.
+     * @param t_colmeta
+     * @return Whether the column meta was successfully added.
+     */
+    template <typename T>
+        requires std::same_as<std::remove_cvref<T>, ColumnMeta>
+    auto add_column(const ColumnMeta& t_colmeta) -> bool {
+        return m_entries.emplace(t_colmeta.m_col_id, std::forward<T>(t_colmeta))
+            .second;
+    }
+    /**
+     * @brief Note; this is marked as noexcept because I believe the default
+     * hash and equality function for size_t don't throw anything.
+     *
+     * @param t_id
+     * @return
+     */
+    auto remove_column(ColID t_id) noexcept -> bool {
+        return m_entries.erase(t_id) == 1;
+    }
     /**
      * @brief Overwrite the content of a file with the table metadata.
      *
      * @param t_path The path to write to.
      */
-    auto write_to(std::filesystem::path t_path);
+    void write_to(const std::filesystem::path& t_path);
 
   private:
-    std::unordered_map<ColID, ColumnMeta> m_entries;
+    std::map<ColID, ColumnMeta> m_entries;
     std::string m_name;
 };
 
