9792453b53804760c832ecbba9450d9a245fd1b1
Huy Nguyen <huy55465@gmail.com>
2024-12-26 22:56:30 -0500

    fix: move cursor to read free page
    I forgot to do that. So, there's a chance it's reading at random places.
    
    commit-by: Huy Nguyen

--- src/dbfile/page_free.cxx
+++ src/dbfile/page_free.cxx
@@ -1,12 +1,13 @@
 #include "page.hxx"
 #include <bit>
+#include <iostream>
 
 namespace tinydb::dbfile {
 
 void FreePageMeta::write_to(std::ostream& t_out) {
     t_out.seekp(m_pagenum * PAGESIZ);
 
-    t_out << static_cast<uint8_t>(0);
+    t_out << static_cast<pt_num>(PageType::Free);
     // it seems that if I don't do this, it automatically writes one byte if
     // m_n_pages is 1 digit long, even though we want 4.
     //
@@ -14,10 +15,12 @@ void FreePageMeta::write_to(std::ostream& t_out) {
     // delimiter (comma, semicolon) to determine how long a variable is.
     t_out.write(std::bit_cast<const char*>(&m_n_pages), sizeof(m_n_pages));
     t_out.write(std::bit_cast<const char*>(&m_p_next_page),
-               sizeof(m_p_next_page));
+                sizeof(m_p_next_page));
 }
 
 void FreePageMeta::read_from(std::istream& t_in) {
+    t_in.seekg(m_pagenum * PAGESIZ);
+
     uint8_t pagetype{0};
     t_in >> pagetype;
     if (pagetype != static_cast<pt_num>(PageType::Free)) {
@@ -30,7 +33,7 @@ void FreePageMeta::read_from(std::istream& t_in) {
     t_in.rdbuf()->sgetn(std::bit_cast<char*>(&npages), sizeof(npages));
 
     uint32_t pnext{0};
-    t_in.rdbuf()->sgetn(std::bit_cast<char*>(&pnext), sizeof(npages));
+    t_in.rdbuf()->sgetn(std::bit_cast<char*>(&pnext), sizeof(pnext));
 
     m_n_pages = npages;
     m_p_next_page = pnext;
