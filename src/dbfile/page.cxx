d72561699472e731bfef241f40def8ffe5bc6049
Huy Nguyen <huy55465@gmail.com>
2024-12-29 01:31:26 -0500

    write out btree internal defn, not tested yet
    I dunno if I wanna test this tbh. It's almost the same as FreePageMeta.
    
    commit-by: Huy Nguyen

--- src/dbfile/page.cxx
+++ src/dbfile/page.cxx
@@ -39,4 +39,31 @@ void FreePageMeta::read_from(std::istream& t_in) {
     m_p_next_page = pnext;
 }
 
+void BTreeLeafMeta::write_to(std::ostream& t_out) {
+    t_out.seekp(m_pagenum * PAGESIZ);
+
+    t_out << static_cast<pt_num>(PageType::BTreeLeaf);
+    t_out.write(std::bit_cast<const char*>(&m_n_cols), sizeof(m_n_cols));
+    t_out.write(std::bit_cast<const char*>(&m_p_next), sizeof(m_p_next));
+}
+
+void BTreeLeafMeta::read_from(std::istream& t_in) {
+    t_in.seekg(m_pagenum * PAGESIZ);
+
+    uint8_t pagetype{0};
+    t_in >> pagetype;
+    if (pagetype != static_cast<pt_num>(PageType::BTreeLeaf)) {
+        throw PageReadError::WrongPageType;
+    }
+
+    uint16_t ncols{0};
+    t_in.rdbuf()->sgetn(std::bit_cast<char*>(&ncols), sizeof(ncols));
+
+    uint32_t pnextpg{0};
+    t_in.rdbuf()->sgetn(std::bit_cast<char*>(&pnextpg), sizeof(pnextpg));
+
+    m_n_cols = ncols;
+    m_p_next = pnextpg;
+}
+
 } // namespace tinydb::dbfile
