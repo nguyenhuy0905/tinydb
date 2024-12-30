4824cebad1ce0e97959c596fd5270a767692997f
Huy Nguyen <huy55465@gmail.com>
2024-12-29 23:07:07 -0500

    fix: file not created
    If I delete the test file, the thing isn't automatically created again.
    
    commit-by: Huy Nguyen

--- unittest/unit-test.cxx
+++ unittest/unit-test.cxx
@@ -10,7 +10,7 @@ TEST(some, tbl_test) {
     std::println("This test is meant to be used with GDB");
     using namespace tinydb::dbfile;
     TableMeta tbl{"some-tbl"};
-    std::fstream file{"test.db"};
+    std::fstream file{"test.db", std::fstream::in | std::fstream::out};
     tbl.add_column(
         ColumnMeta{.m_name{"col1"}, .m_size = 4, .m_col_id = 1, .m_offset = 0});
     tbl.add_column(ColumnMeta{
