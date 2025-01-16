956b37a04d22ece4b239a130db05b55218da1f04
Huy Nguyen <huy55465@gmail.com>
2025-01-15 21:49:56 -0500

    fix: use rdbuf for writing instead of << overload
    This type of format is actual L. std::format FTW.
    
    commit-by: Huy Nguyen

--- src/dbfile/tbl.cxx
+++ src/dbfile/tbl.cxx
@@ -24,21 +24,42 @@ namespace tinydb::dbfile {
 void TableMeta::write_to(std::ostream& t_out) {
     t_out.seekp(TBL_OFF);
     // table format:
-    // tblname{col1-name,col1-id,col1-type,col1-size,col1-offset;...;coln-name,coln-id,coln-type,coln-size,coln-offset;}
+    // tblname{col1-name,col1-idcol1-typecol1-sizecol1-offsetcol2-name,col2-id...coln-name,coln-id,coln-type,coln-size,coln-offset;}
+    //
+    // Basically:
+    // - anything from TBL_OFF to before { is the table name.
+    // - Anything after the { until before , is first column's name.
+    // - id, type, size, offset are all numbers. So, the size is their bound.
+    // - Anything after the last byte of column 1's offset and before the next ,
+    // is column 2's name.
+    // - Rinse and repeat from here.
 
-    // id is NOT written as characters, due to how this thing works. uint8_t
-    // gets interpreted as bytes. With this implementation, the names cannot
-    // contain any comma, semicolon or curly brace.
+    auto& rdbuf = *t_out.rdbuf();
+    auto write = [&]<typename T>(const T& t_val) -> std::streamsize {
+        return rdbuf.sputn(std::bit_cast<const char*>(&t_val), sizeof(T));
+    };
+    auto write_string = [&](const std::string& t_str) -> std::streamsize {
+        return rdbuf.sputn(t_str.c_str(),
+                           static_cast<std::streamsize>(t_str.length()));
+    };
 
-    // maybe I should throw some kind of failbit exception here.
-    t_out << m_name << '{' << m_key << ';';
+    write_string(m_name);
+    write('{');
+    write_string(m_key);
+    write(';');
     for (const ColumnMeta& colmeta :
          m_entries | std::views::transform(
                          [](const auto& pair) { return pair.second; })) {
         auto type_id = column::type_id(colmeta.m_type);
         auto typesiz = column::type_size(colmeta.m_type);
-        t_out << colmeta.m_name << ',' << colmeta.m_col_id << ',' << type_id
-              << ',' << typesiz << ',' << colmeta.m_offset << ';';
+        write_string(colmeta.m_name);
+        rdbuf.sputc(',');
+        // with this, I don't really need the commas anymore.
+        // The numbers' bounds are their sizes.
+        write(colmeta.m_col_id);
+        write(type_id);
+        write(typesiz);
+        write(colmeta.m_offset);
     }
     t_out << '}';
 }
@@ -54,37 +75,43 @@ auto TableMeta::read_from(std::istream& t_in) -> TableMeta {
     // After that, put formatted data into the variable passed in, then clear
     // the stringstream flags and content.
 
-    auto fill_var = [&]<typename T, typename... Args>(char delim,
-                                                      Args... t_args) -> T {
-        std::stringstream sbuilder{};
-        T var{t_args...};
-        t_in.get(*sbuilder.rdbuf(), delim);
-        sbuilder >> var;
+    auto fill_string = [&](char delim) -> std::string {
+        std::stringstream var{};
+        while (t_in.peek() != delim) {
+            var.put(static_cast<char>(t_in.get()));
+        }
         t_in.seekg(1, std::ios_base::cur);
+        return var.str();
+    };
+
+    auto fill_num =
+        [&]<typename T, typename... Args>(Args... t_placeholders) -> T {
+        T var{t_placeholders...};
+        t_in.rdbuf()->sgetn(std::bit_cast<char*>(&var), sizeof(T));
         return var;
     };
 
-    auto tblname = fill_var.operator()<std::string>('{');
+    auto tblname = fill_string('{');
     TableMeta new_tbl{std::move(tblname)};
 
-    auto key = fill_var.operator()<std::string>(';');
+    auto key = fill_string(';');
 
     while (t_in.peek() != '}') {
-        auto colname = fill_var.operator()<std::string>(',');
+        auto colname = fill_string(',');
 
-        auto id = fill_var.operator()<ColID>(',');
+        auto id = fill_num.operator()<ColID>();
 
-        auto typenum = fill_var.operator()<column::IdType>(',');
+        auto typenum = fill_num.operator()<column::IdType>();
 
         auto type = column::type_of(typenum).value();
-        auto size = fill_var.operator()<uint64_t>(',');
+        auto size = fill_num.operator()<uint64_t>();
         type = column::map_type(
             type, [](auto t_scl) { return column::ColType{t_scl}; },
             [&](column::TextType& _) mutable {
                 return column::ColType{column::TextType{size}};
             });
 
-        auto off = fill_var.operator()<uint8_t>(';');
+        auto off = fill_num.operator()<uint8_t>();
 
         auto new_col = ColumnMeta{.m_name{std::move(colname)},
                                   .m_type = type,
--- unittest/tbl-test.cxx
+++ unittest/tbl-test.cxx
@@ -23,7 +23,21 @@ TEST(tbl, init) {
     tbltest.set_key("col1");
     tbltest.write_to(test_stream);
     auto readtest = TableMeta::read_from(test_stream);
-    // breakpoint here and test
+    // breakpoint here to inspect variables
+    // if get_column or read_from is faulty, this part here throws an exception.
+    auto initial_col2 = tbltest.get_column("col2").value().get();
+    auto read_col2 = readtest.get_column("col2").value().get();
+
+    ASSERT_EQ(initial_col2.m_name, read_col2.m_name);
+    ASSERT_EQ(initial_col2.m_offset, read_col2.m_offset);
+    ASSERT_EQ(column::type_id(initial_col2.m_type),
+              column::type_id(read_col2.m_type));
+    ASSERT_EQ(column::type_id(initial_col2.m_type),
+              column::type_id(column::TextType{}));
+    // here it's safe to yank out TextType in both of these columns.
+    auto initial_txt = std::get<column::TextType>(initial_col2.m_type);
+    auto read_txt = std::get<column::TextType>(read_col2.m_type);
+    ASSERT_EQ(initial_txt.get_size(), read_txt.get_size());
 }
 #else
 import std;
