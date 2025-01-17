fe7c56a4ae5a006d3213a8f689dfc8215a4cd8d9
Huy Nguyen <huy55465@gmail.com>
2025-01-16 21:29:35 -0500

    re-add dbfile test
    This is a more reasonable place for that file.
    
    commit-by: Huy Nguyen

---
+++ src/dbfile/test/dbfile_test.cxx
@@ -0,0 +1,31 @@
+#ifndef ENABLE_MODULE
+#include "dbfile/coltype.hxx"
+#include "dbfile/dbfile.hxx"
+#include "general/sizes.hxx"
+#include "dbfile/internal/tbl.hxx"
+#include <gtest/gtest.h>
+#include <sstream>
+TEST(dbfile, init) {
+    using namespace tinydb::dbfile;
+    using namespace tinydb::dbfile::internal;
+    auto test_stream = std::make_unique<std::stringstream>(
+        std::string(2 * tinydb::SIZEOF_PAGE, '\0'));
+    test_stream->exceptions(std::stringstream::failbit);
+    // i got an exception trying to write into this without creating and writing
+    // the table in first. Which is actually a good thing. Better than some
+    // random data being written.
+    TableMeta meta{"tbl1"};
+    meta.add_column(ColumnMeta{.m_name{"col1"},
+                               .m_type{column::ScalarColType::Int32},
+                               .m_col_id = 1,
+                               .m_offset = 0});
+    meta.set_key("col1");
+    meta.write_to(*test_stream);
+    auto dbfile = DbFile::construct_from(std::move(test_stream));
+    // create a breakpoint here and check.
+    // You know, I don't want to provide an equality operator just to assert.
+}
+#else
+import tinydb.dbfile;
+// #include "sizes.hxx"
+#endif // !ENABLE_MODULE
