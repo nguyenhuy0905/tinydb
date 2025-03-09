475964739426b3483c809b824e462759fac63c2a
Huy Nguyen <huy55465@gmail.com>
2025-03-09 16:48:12 -0400

    fix(dbfile): forget to track heap_base
    Commit-by: Huy Nguyen

---
+++ src/dbfile/internal/heap_base.cxx
@@ -0,0 +1,12 @@
+#ifdef ENABLE_MODULES
+module;
+#include <cassert>
+#include <cstddef>
+#include <utility>
+#include <variant>
+#include <type_traits>
+export module tinydb.dbfile.internal.heap_base;
+import tinydb.dbfile.internal.page_base;
+
+#include "dbfile/internal/heap_base.hxx"
+#endif // ENABLE_MODULES
