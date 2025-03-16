baf1a1fd434455df73d31f246a3514e9f0fc9e84
Huy Nguyen <huy55465@gmail.com>
2025-03-16 18:36:32 -0400

    wip(token): more test cases

--- src/stmt/token.test.cpp
+++ src/stmt/token.test.cpp
@@ -23,20 +23,101 @@ TEST(Tokenizer, Empty) {
   EXPECT_TRUE(std::move(test_data).move_token_list().empty());
 }
 
-TEST(Tokenizer, OneNum) {
-  TokenizerData test_data{"34;"sv};
-  Tokenizer tok{};
-  auto tok_result = [&]() {
-    auto ret = tok(test_data);
-    while (ret) {
-      ret = (*ret)(test_data);
-    }
-    return ret;
-  }();
-  auto tok_list = std::move(test_data).move_token_list();
-  EXPECT_FALSE(tok_list.empty());
-  EXPECT_EQ(tok_list.front().lexeme, "34"sv);
-  EXPECT_EQ(tok_list.front().type, TokenType::Number);
-  EXPECT_FALSE(tok_result);
-  EXPECT_EQ(tok_result.error().type, ParseError::ErrType::Done);
+TEST(Tokenizer, Num) {
+  {
+    TokenizerData test_data{"34;"sv};
+    Tokenizer tok{};
+    auto tok_result = [&]() {
+      auto ret = tok(test_data);
+      while (ret) {
+        ret = (*ret)(test_data);
+      }
+      return ret;
+    }();
+    auto tok_list = std::move(test_data).move_token_list();
+    EXPECT_FALSE(tok_list.empty());
+    EXPECT_EQ(tok_list.front().lexeme, "34"sv);
+    EXPECT_EQ(tok_list.front().type, TokenType::Number);
+    EXPECT_FALSE(tok_result);
+    EXPECT_EQ(tok_result.error().type, ParseError::ErrType::Done);
+  }
+  {
+    TokenizerData test_data{"12.34;"sv};
+    Tokenizer tok{};
+    auto tok_result = [&]() {
+      auto ret = tok(test_data);
+      while (ret) {
+        ret = (*ret)(test_data);
+      }
+      return ret;
+    }();
+    auto tok_list = std::move(test_data).move_token_list();
+    EXPECT_FALSE(tok_list.empty());
+    EXPECT_FALSE(tok_result);
+    EXPECT_EQ(tok_result.error().type, ParseError::ErrType::Done);
+    EXPECT_EQ(tok_list.front().lexeme, "12.34"sv);
+    EXPECT_EQ(tok_list.front().type, TokenType::Number);
+  }
+  {
+    TokenizerData test_data{"12.34 -56;"sv};
+    Tokenizer tok{};
+    auto tok_result = [&]() {
+      auto ret = tok(test_data);
+      while (ret) {
+        ret = (*ret)(test_data);
+      }
+      return ret;
+    }();
+    auto tok_list = std::move(test_data).move_token_list();
+    EXPECT_FALSE(tok_list.empty());
+    EXPECT_FALSE(tok_result);
+    EXPECT_EQ(tok_result.error().type, ParseError::ErrType::Done);
+    // NOLINTBEGIN(*unused-return-value*)
+    EXPECT_NO_THROW(tok_list.at(0));
+    EXPECT_NO_THROW(tok_list.at(1));
+    EXPECT_NO_THROW(tok_list.at(2));
+    // NOLINTEND(*unused-return-value*)
+    EXPECT_EQ(tok_list.at(0).lexeme, "12.34"sv);
+    EXPECT_EQ(tok_list.at(1).lexeme, "-"sv);
+    EXPECT_EQ(tok_list.at(1).type, TokenType::Minus);
+    EXPECT_EQ(tok_list.at(2).lexeme, "56"sv);
+    EXPECT_EQ(tok_list.front().type, TokenType::Number);
+  }
+}
+
+TEST(Tokenizer, KwAndId) {
+  {
+    TokenizerData test_data{"hEllo;"sv};
+    Tokenizer tok{};
+    auto tok_result = [&]() {
+      auto ret = tok(test_data);
+      while (ret) {
+        ret = (*ret)(test_data);
+      }
+      return ret;
+    }();
+    auto tok_list = std::move(test_data).move_token_list();
+    EXPECT_FALSE(tok_list.empty());
+    EXPECT_FALSE(tok_result);
+    EXPECT_EQ(tok_result.error().type, ParseError::ErrType::Done);
+    EXPECT_EQ(tok_list.front().lexeme, "hEllo"sv);
+    EXPECT_EQ(tok_list.front().type, TokenType::Identifier);
+  }
+  {
+    TokenizerData test_data{"and;"sv};
+    Tokenizer tok{};
+    auto tok_result = [&]() {
+      auto ret = tok(test_data);
+      while (ret) {
+        ret = (*ret)(test_data);
+      }
+      return ret;
+    }();
+    auto tok_list = std::move(test_data).move_token_list();
+    EXPECT_FALSE(tok_list.empty());
+    EXPECT_FALSE(tok_result);
+    EXPECT_EQ(tok_result.error().type, ParseError::ErrType::Done);
+    EXPECT_EQ(tok_list.front().lexeme, "and"sv);
+    EXPECT_EQ(tok_list.front().type, TokenType::And);
+  }
 }
